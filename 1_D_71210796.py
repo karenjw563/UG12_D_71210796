f6e7ab9aa6eb092bd5e87ee2b42d119490414f81
karenjw563 <93313863+karenjw563@users.noreply.github.com>
2022-12-08 17:23:23 +0700

    Add files via upload

--- 1_D_71210796.py
+++ 1_D_71210796.py
@@ -9,49 +9,47 @@ roti = 15000
 indomie = 3000
 vitamin = 50000
 
-tanya = str(input("Apa barang yang akan Anda beli? "))
+if mulaiBeli.lower() == 'start':
 
-mb = str.lower(mulaiBeli)
-
-if start.lower() == 'start':
     while True:
-        beli = input('beli? ')
-
-        if beli.lower() =='susu':
-            if uangAwal >= susu:
-                uangAwal -= susu
-                print("Sisa uang anda"+ str(masukUang-susu))
+        tanya = str(input("Apa barang yang akan Anda beli? "))
+        if tanya.lower() =='susu':
+            if masukUang >= susu:
+                masukUang -= susu
+                print("Sisa uang Anda", masukUang)
             else:
                 print("Uang anda tidak cukup")
 
-        elif beli.lower() =='permen':
-            if uangAwal >= permen:
-                uangAwal -= permen
-                print("Sisa uang anda"+ str(masukUang-permen))
+        elif tanya.lower() =='permen':
+            if masukUang >= permen:
+                masukUang -= permen
+                print("Sisa uang Anda", masukUang)
             else:
                 print("Uang anda tidak cukup")
 
-        elif beli.lower() =='roti':
-            if uangAwal >= roti:
-                uangAwal -= roti
-                print("Sisa uang anda"+ str(masukUang-roti))
+        elif tanya.lower() =='roti':
+            if masukUang >= roti:
+                masukUang -= roti
+                print("Sisa uang Anda", masukUang)
             else:
                 print("Uang anda tidak cukup")
 
-        elif beli.lower() =='indomie':
-            if uangAwal >= indomie:
-                uangAwal -= indomie
-                print("Sisa uang anda"+ str(masukUang-indomie))
+        elif tanya.lower() =='indomie':
+            if masukUang >= indomie:
+                masukUang -= indomie
+                print("Sisa uang Anda", masukUang)
             else:
                 print("Uang anda tidak cukup")
 
-        elif beli.lower() =='vitamin':
-            if uangAwal >= vitamin:
-                uangAwal -= vitamin
-                print("Sisa uang anda"+ str(masukUang-vitamin))
+        elif tanya.lower() =='vitamin':
+            if masukUang >= vitamin:
+                masukUang -= vitamin
+                print("Sisa uang Anda", masukUang)
             else:
                 print("Uang anda tidak cukup")
-        elif beli.lower() == 'sudah':
+
+        elif tanya.lower() == 'sudah':
             break
-    else:
-        print("Barang tidak tersedia")
+
+        else:
+            print("Barang tidak tersedia")
